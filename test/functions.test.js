fd83ed2d84bae117b561d1da0a7451fa37064971
fmtabbara <fmtabbara@hotmail.co.uk>
2021-10-23 20:20:36 +0100

    new test

--- test/functions.test.js
+++ test/functions.test.js
@@ -2,10 +2,16 @@ import { strict as assert } from 'assert'
 import { map } from '../functions.js'
 
 describe('function tests', () => {
-  it('should add one to each item in the array', () => {
+  it('should add one to each number in the array', () => {
     const original = [4, 5, 6]
     const mapped = map(original, (i) => i + 1)
 
     return assert.deepEqual(mapped, [5, 6, 7])
+  })
+  it('should subtract one from each number in the array', () => {
+    const original = [4, 5, 6]
+    const mapped = map(original, (i) => i - 1)
+
+    return assert.deepEqual(mapped, [3, 4, 5])
   })
 })

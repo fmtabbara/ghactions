63d85ba24b4217fe844a55846aac365af4713f01
fmtabbara <fmtabbara@hotmail.co.uk>
2021-10-23 20:42:02 +0100

    fix syntax error

--- test/functions.test.js
+++ test/functions.test.js
@@ -13,7 +13,7 @@ describe('function tests', () => {
     const mapped = map(original, (i) => i - 1)
 
     return assert.deepEqual(mapped, [3, 4, 5])
-
+  })
   it('should add ten to each item in the array', () => {
     const original = [4, 5, 6]
     const mapped = map(original, (i) => i + 10)

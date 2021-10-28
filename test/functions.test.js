e1d588310828068bab7828be0585acd249ca8766
fmtabbara <fmtabbara@hotmail.co.uk>
2021-10-28 23:44:32 +0100

    add failing test to check deployment

--- test/functions.test.js
+++ test/functions.test.js
@@ -29,6 +29,6 @@ describe("function tests", () => {
   it("should add two numbers", () => {
     const result = add(1, 2);
 
-    return assert.deepEqual(result, 3);
+    return assert.deepEqual(result, 4);
   });
 });
